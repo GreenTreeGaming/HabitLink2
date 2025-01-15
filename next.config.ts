afae4436ba84217c97b234b24acaa3ec2bd0806c
Sarvajith Karun <obbyslicedyt@gmail.com>
2025-01-15 03:37:36 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
 async function triggerScheduler() {
-  if (typeof window === "undefined") {
-    // Make a request to initialize the scheduler
-    await fetch("http://localhost:3000/api/scheduler").catch((err) => {
+ if (typeof window === "undefined") {
+    const baseUrl =
+      process.env.VERCEL_URL || process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+    try {
+      // Use the base URL to make a request
+      await fetch(`${baseUrl}/api/scheduler`);
+    } catch (err) {
       console.error("Failed to initialize scheduler:", err);
-    });
+    }
   }
 }
 
