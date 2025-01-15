831956c9ab8e551caf26fcee4e89c5904480102e
Sarvajith Karun <obbyslicedyt@gmail.com>
2025-01-15 07:27:42 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options */
   ignoreBuildErrors: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
 };
 
 const triggerScheduler = async () => {
